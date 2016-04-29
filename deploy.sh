b49e8b08b4bf911d869216cf8165fa73944f2b34
Adam Goldman <adamgoldman7@gmail.com>
2016-04-30 02:16:41 +0300

    6

--- deploy.sh
+++ deploy.sh
@@ -49,15 +49,15 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt travis-keys.enc
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis-keys.pub.enc -out travis-keys -d
-chmod 600 travis-keys
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.pub.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add travis-keys
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
