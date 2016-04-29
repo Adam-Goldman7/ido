0128ab8f617de537426788674a6f36748d9518f6
Adam Goldman <adamgoldman7@gmail.com>
2016-04-30 01:17:36 +0300

    go go!

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir out
+cp -a . out
+rm -rf out/out
--- deploy-to-buzz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/deploy_key.pem # this key should have push access
-ssh-add .travis/deploy_key.pem
-git remote add deploy git@github.com:Adam-Goldman7/ido.git
-git push deploy master:buzz
