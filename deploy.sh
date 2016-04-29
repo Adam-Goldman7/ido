9a40217f922e3676117d43946b89ea72ebcfa745
Adam Goldman <adamgoldman7@gmail.com>
2016-04-30 02:00:30 +0300

    3

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis-keys.enc -out travis-keys -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis-keys.pub.enc -out travis-keys -d
 chmod 600 travis-keys
 eval `ssh-agent -s`
 ssh-add travis-keys
